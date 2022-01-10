0a01a105302a3014de211224dcba94fcca8e4aed
Nithya Natarajan <nithyanatarajn@gmail.com>
2022-01-10 14:38:46 +0530

    Accomodate ozzo rules with ctx at field level

--- pkg/models/person.go
+++ pkg/models/person.go
@@ -14,8 +14,18 @@ type Person struct {
 
 func (p Person) ValidateWithContext(ctx context.Context) error {
 	return validation.ValidateStructWithContext(ctx, &p,
-		validation.Field(&p.Age, validation.Required, validation.WithContext(AgeLimitCustomRuleDefinition())),
+		//validation.Field(&p.Age, validation.Required, validation.WithContext(AgeLimitCustomRuleDefinition())),
+		validation.Field(&p.Age, AgeLimitCustomRuleDefinitionRules(ctx)...),
 	)
+}
+
+func AgeLimitCustomRuleDefinitionRules(ctx context.Context) (rules []validation.Rule) {
+	rules = append(rules, validation.Required)
+	if data, ok := ctx.Value("ageLimit").(string); ok {
+		ageLimit, _ := strconv.Atoi(data)
+		rules = append(rules, validation.Max(ageLimit).Error("person's age limit reached"))
+	}
+	return
 }
 
 func AgeLimitCustomRuleDefinition() validation.RuleWithContextFunc {
